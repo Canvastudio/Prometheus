11ce62bff603b0eebf7ea145501b386f1082ac22
青鑫 <amostalong@126.com>
2017-09-13 00:46:49 +0800

    青鑫：9月12一些基本得代码。

---
+++ Code/Prometheus/Assets/Scripts/Logical/Brick.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+#region BrickInfo
+    public BrickType brickType
+    {
+        get
+        {
+            return _brickType;
+        }
+    }
+
+    private BrickType _brickType;
+
+    public BrickExplored brickExplored
+    {
+        get
+        {
+            return _brickExplored;
+        }
+    }
+
+    private BrickExplored _brickExplored = BrickExplored.UNEXPLORED;
+
+    public BrickBlock brickBlock
+    {
+        get
+        {
+            return _brickBlock;
+        }
+    }
+
+    private BrickBlock _brickBlock = BrickBlock.NIL;
+#endregion
+
+
+}
+
+public enum BrickType
+{
+    EMPTY,
+    OBSTACLE,
+    MONSTER,
+    TRADER,
+    OTHERS,
+}
+
+public enum BrickExplored
+{
+    UNEXPLORED = 0,
+    EXPLORED = 1,
+}
+
+public enum BrickBlock
+{
+    NIL = 0,
+    BLOCK_OTHER = 1,
+    BLOCKED_BY_OTHER = 2,
+}
+
+
---
+++ Code/Prometheus/Assets/Scripts/Logical/GameStateMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateMachine : SingleObject<GameStateMachine> {
+    
+    public enum GameState
+    {
+        NIL,
+        LOADING,
+        INIT,
+        PLAYER,
+        MONSTER,
+        MAP,
+        END,
+        PAUSE,
+    }
+
+    public GameState gameState
+    {
+        get
+        {
+            return _gameState;
+        }
+    }
+
+    private GameState _gameState;
+
+    protected override void Init()
+    {
+
+    }
+
+    private void SwitchGameState(GameState nextState)
+    {
+
+    }
+}
