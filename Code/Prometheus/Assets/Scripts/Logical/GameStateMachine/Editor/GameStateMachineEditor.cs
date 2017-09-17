8a3b79a3a7b04ad6c8e094162293494a84fa2315
青鑫 <amostalong@126.com>
2017-09-17 23:37:13 +0800

    青鑫：准备搞一个Graph来生成GameStateMachine

---
+++ Code/Prometheus/Assets/Scripts/Logical/GameStateMachine/Editor/GameStateMachineEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GameStateMachineEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Open State Graph", "LargeButton"))
+        {
+
+        }
+    }
+}
