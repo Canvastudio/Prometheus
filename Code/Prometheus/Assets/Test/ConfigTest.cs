86da61ad2ca0188f26ea89e3d3f8dfda10e90d26
Shu.Zhang <403111902@qq.com>
2017-09-20 00:52:05 +0800

    配置表工具现在可以自己添加路径，不需要unity添加了。另外，我新建了一个Configtest场景，用来检查配置表更新后有没有错误的

---
+++ Code/Prometheus/Assets/Test/ConfigTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SuperConfig.Instance.Load();
+        Debug.Log(StateConfig.GetConfigDataById<StateConfig>(4000001).name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
