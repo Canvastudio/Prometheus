fe4f4d1d899a24e52e4ef1a7fc921a2bebd5cb52
Shu.Zhang <403111902@qq.com>
2017-09-21 00:24:07 +0800

    再来一次

--- Code/Prometheus/Assets/Test/ConfigTest.cs
+++ Code/Prometheus/Assets/Test/ConfigTest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class B : MonoBehaviour {
+public class ConfigTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
