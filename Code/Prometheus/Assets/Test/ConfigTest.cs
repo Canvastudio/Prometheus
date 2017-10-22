a7f921c3206bbf5790686bfbf09c7cc660a8a206
Shu.Zhang <403111902@qq.com>
2017-10-22 22:48:22 +0800

    技能数据处理

--- Code/Prometheus/Assets/Test/ConfigTest.cs
+++ Code/Prometheus/Assets/Test/ConfigTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class ConfigTest : MonoBehaviour {
@@ -7,23 +8,50 @@ public class ConfigTest : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         //SuperConfig.Instance.Load(SuperTool.GetConfigData("DefaultPath.txt", "Config"));
-	    //SuperConfig.Instance.Load();
-        //Debug.Log(ChipDiskConfig.GetConfigDataById<ChipDiskConfig>(1).chipGridMatrix[0,0]);
+        SuperTimer.Instance.CreatAndBound(this);
+        //SuperTimer.Instance.CoroutineStart(Check(),this);
+        //SuperConfig.Instance.Load();
 
-        SuperArrayValue<ChipGrid> tt=new SuperArrayValue<ChipGrid>("Normal;Normal|Normal;Normal", "|;");
-	    Debug.Log(tt[0,0]);
+	    //var t = new SuperArrayValue<long>("2199023255553,1103806595087,2199023255652", ",");
+	    //Debug.Log(t[0]);
 
-	    //SuperTool.ToRpn()
+	    string t = "\"2199023255553,1103806595087,2199023255652,1103806595073,1103806595087,2199023255653,1103806595073,1103806595076,1103806595074,2199023255602,1103806595075\"";
 
-	    //var t=SuperTool.CreateWeightSection(new List<int> { 1, 2, 3 });
-	    //   Debug.Log(t.RanPoint());
-	    //var t = new SuperArrayValue<int>("1:2|3",":|");
-	    //   Debug.Log(t[0,0]);
+	    Debug.Log(ReplaceQuote(t));
+	    //ReplaceQuote("");
 
+
+
+	    //Debug.Log(ActiveSkillsConfig.GetConfigDataById<ActiveSkillsConfig>(1000007).activeSkillArgs[0].u[0]);
+
+	    //Debug.Log(ChipDiskConfig.GetConfigDataById<ChipDiskConfig>(1).chipGridMatrix[0,0]);
+
+	    //Debug.Log((ChipConfig.GetConfigDataById<ChipConfig>(100226).propertyAddition.gamePropertys[0]).GetType() == typeof (GameProperty));
+
+	    //var t = new SuperArrayValue<Stuff>("Coherer;Organics", ";");
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
+
+    private IEnumerator Check()
+    {
+        yield return SuperConfig.Instance.LoadAsync();
+        Debug.Log(ModuleConfig.GetConfigDataById<ModuleConfig>(1000001));
+    }
+
+    private  string ReplaceQuote(string value)
+    {
+        if (value == null) return null;
+        if (value == "") return "";
+        Regex reg = new Regex(@"^"".*""$");
+        if (reg.IsMatch(value)) value = value.Substring(1, value.Length - 2);
+        Regex reg2 = new Regex(@"[\r\n]");
+        value = reg2.Replace(value, "");
+        return value.Trim();
+    }
+
+
 }
