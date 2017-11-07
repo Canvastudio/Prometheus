33d2f9836dd0d849fe898d5a4aac28f41005d0e8
Canvas <444569546@qq.com>
2017-11-08 00:54:10 +0800

    优化技能表现接口

---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/Manager/ArtSkill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtSkill {
+
+	public static void DoSkill(string name, Vector3 _start_pos, Vector3 _end_pos, Callback _callback = null) {
+	
+		GameObject objskill = FxPool.Get(FxEnum.Skill, name);
+		objskill.GetComponent<ArtEventFlow>().Init(_start_pos, _end_pos, _callback);
+	
+	}
+
+	public static void DoSkill(string name, Vector3 _start_pos, Callback _callback = null) {
+	
+		GameObject objskill = FxPool.Get(FxEnum.Skill, name);
+		objskill.GetComponent<ArtEventFlow>().Init(_start_pos, _start_pos, _callback);
+	
+	}
+
+}
--- Code/Prometheus/Assets/Art/Fx/Scripts/TCode/ArtTestSkill.cs
+++ Code/Prometheus/Assets/Art/Fx/Scripts/TCode/ArtTestSkill.cs
@@ -7,19 +7,13 @@ public class ArtTestSkill : MonoBehaviour {
 	public List<Transform> tranlist;
 
 	public string skill_name = "liudan";
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetKeyDown(KeyCode.F)) {
-		
-			GameObject objskill = FxPool.Get(FxEnum.Skill, skill_name);
-			objskill.GetComponent<ArtEventFlow>().Init(tranlist[0].position, tranlist[1].position, null);
+
+			ArtSkill.DoSkill(skill_name, tranlist[0].position, tranlist[1].position, null);
 		
 		}
 		
