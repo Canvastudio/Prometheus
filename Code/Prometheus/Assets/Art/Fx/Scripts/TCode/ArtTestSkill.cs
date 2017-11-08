ca60bbc938a9b719020be6b0a82281c5244fc7b3
Canvas <444569546@qq.com>
2017-11-08 17:30:49 +0800

    更改技能测试输入

--- Code/Prometheus/Assets/Art/Fx/Scripts/TCode/ArtTestSkill.cs
+++ Code/Prometheus/Assets/Art/Fx/Scripts/TCode/ArtTestSkill.cs
@@ -7,15 +7,69 @@ public class ArtTestSkill : MonoBehaviour {
 	public List<Transform> tranlist;
 
 	public string skill_name = "liudan";
+
+	public List<string> skilllist = new List<string> {
+
+		"none",
+		"none",
+		"nangua",
+		"liudan",
+		"guanchuanshexian",
+		"quandanqifa",
+		"sanshe",
+		"qiyoudan",
+		"xuanguangshexian",
+		"jingshencuoluan",
+		"bianxing",
+		"nuyi",
+		"cuimian",
+		"chenmo",
+		"jinghuashuiyue",
+		"shengmingchouqu",
+		"zhongzidan",
+		"ganraodan",
+	
+	};
+
+	public string numstring = "";
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown(KeyCode.F)) {
+//		if (Input.GetKeyDown(KeyCode.F)) {
+//
+//			ArtSkill.DoSkill(skill_name, tranlist[0].position, tranlist[1].position, null);
+//		
+//		}
 
-			ArtSkill.DoSkill(skill_name, tranlist[0].position, tranlist[1].position, null);
+		if (Input.GetKeyDown(KeyCode.KeypadEnter)) {
+		
+			int number = 0;
+
+			if (int.TryParse(numstring, out number)) {
+			
+				if (number > 1 && number < skilllist.Count) {
+					skill_name = skilllist[number];
+					ArtSkill.DoSkill(skill_name, tranlist[0].position, tranlist[1].position, null);
+				}
+			
+			}
+
+
+			numstring = "";
+			return;
 		
 		}
+
+
+		if (Input.anyKeyDown) {
+			
+			numstring += Input.inputString;
+			Debug.Log(numstring);
+		
+		}
+
+
 		
 	}
 }
