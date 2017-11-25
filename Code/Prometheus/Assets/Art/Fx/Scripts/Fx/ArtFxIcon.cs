5cf1df772bd0b12b929db921e29d54bef918db8e
Canvas <444569546@qq.com>
2017-11-25 23:55:19 +0800

    添加 偏转涂层技能特效。

---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/Fx/ArtFxIcon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtFxIcon : ArtFxBase {
+
+	public Texture tex;
+
+	public override void Init(Transform _tran_start, Transform _tran_end, Callback _OnHit)
+	{
+		base.Init(_tran_start, _tran_end, _OnHit);
+
+		ParticleSystemRenderer render = this.GetComponent<ParticleSystemRenderer>();
+		render.material.mainTexture = tex;
+
+	}
+
+}
