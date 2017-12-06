d2e0331fed3975c05140e22890c65b3e977c1922
Canvas <444569546@qq.com>
2017-12-06 12:00:18 +0800

    添加路点特效。

---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/Fx/ArtLineRollMat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtLineRollMat : MonoBehaviour {
+
+	public Material mat;
+	public Vector2 rollVec2;
+	public float roll_speed = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+		mat = this.GetComponent<LineRenderer>().sharedMaterial;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rollVec2.x -= Time.deltaTime * roll_speed;
+		mat.mainTextureOffset = rollVec2;
+
+	}
+}
---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/Fx/ArtWayPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtWayPoint : MonoBehaviour {
+
+	public LineRenderer lineRender;
+
+	public ParticleSystem endParticle;
+
+	public Vector3[] test_point;
+
+	// Use this for initialization
+	void Start () {
+
+		SetWayPoints(test_point);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetWayPoints(Vector3[] ways) {
+	
+		lineRender.positionCount = ways.Length;
+
+		lineRender.SetPositions(ways);
+
+		endParticle.transform.position = ways[ways.Length - 1];
+	
+	}
+}
