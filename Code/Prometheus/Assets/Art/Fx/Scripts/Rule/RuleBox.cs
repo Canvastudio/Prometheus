bb29714e685c9ff04fe3f27c7cebf0ef70fa3e27
Canvas <444569546@qq.com>
2017-12-09 01:23:54 +0800

    添加 地板规则 脚本。

---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/Rule/RuleBox.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleBox {
+
+	private float rate_normal = 1.0f;
+
+	private static List<string> box_l_list = new List<string>() {
+	
+		"box_l_0",
+		"box_l_1",
+		"box_l_2",
+		"box_l_3",
+		"box_l_4",
+		"box_l_5",
+		"box_l_6",
+	
+	};
+
+	private static List<string> box_h_list = new List<string>() {
+	
+		"box_h_0",
+		"box_h_1",
+	
+	};
+
+	private static List<string> box_d_list = new List<string>() {
+	
+		"box_d_0",
+	
+	};
+
+	private static List<string> box_b_list = new List<string>() {
+	
+		"box_b_0",
+	
+	};
+
+	private static List<string> box_s_list = new List<string>() {
+	
+		"box_s_0",
+	
+	};
+
+
+	public static void GameInit() {
+	
+
+	
+	}
+
+	public static string GetBox(int row, int column) {
+	
+		if (column == 0 || column == 5 && Random.value > 0.95f)
+			return box_s_list[Random.Range(0, box_s_list.Count - 1)];
+
+
+		return box_l_list[Random.Range(0, box_l_list.Count - 1)];
+	
+	}
+
+}
---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/TCode/ArtTestWayPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtTestWayPoint : MonoBehaviour {
+
+	public Vector3[] test_point;
+
+	public ArtWayPoint artWay;
+
+	// Use this for initialization
+	void Start () {
+
+		artWay.SetWayPoints(test_point);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
