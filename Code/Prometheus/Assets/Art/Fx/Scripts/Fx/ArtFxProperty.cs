1c345ab543e269f7d097e89222c2f698fed178d9
Canvas <444569546@qq.com>
2017-12-09 22:24:23 +0800

    更新 唤醒 特效。

---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/Fx/ArtFxProperty.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtFxProperty : MonoBehaviour {
+
+	public ParticleSystem[] particls;
+
+	public void SetSize(float size) {
+	
+		for (int i = 0; i < particls.Length; i++) {
+		
+			ParticleSystem.MainModule main = particls[i].main;
+			main.startSizeMultiplier = size;
+		
+		}
+	
+	}
+
+	public void OnDrawGizmos() {
+
+		SetLayer("StageView");
+
+		particls = this.GetComponentsInChildren<ParticleSystem>(true);
+
+	}
+
+	public string layername = "StageUI";
+
+	public void SetLayer(string _layername) {
+
+		layername = _layername;
+
+		ParticleSystemRenderer[] prenders = this.GetComponentsInChildren<ParticleSystemRenderer>(true);
+
+		for(int i = 0; i < prenders.Length; i++) {
+
+			prenders[i].sortingLayerName = layername;
+
+		}
+
+	}
+
+}
---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/TCode/ArtTestProperty.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtTestProperty : MonoBehaviour {
+
+	public ArtFxProperty artFx;
+
+	// Use this for initialization
+	void Start () {
+
+		artFx.SetSize(Random.Range(3, 10));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
