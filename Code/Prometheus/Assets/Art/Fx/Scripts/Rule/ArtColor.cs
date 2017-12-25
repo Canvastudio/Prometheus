6d7d7fd5876db19baefac1b231f263e7580e3962
Canvas <444569546@qq.com>
2017-12-25 14:43:25 +0800

    提交 颜色 设置脚本。

---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/Rule/ArtColor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtColor : SingleGameObject<ArtColor>  {
+
+	public Color lock_lv;
+	public Color unlock_lv;
+	public float lock_skill_group_alpha = .5f;
+	public float unlock_skill_group_aplha = 1;
+
+}
