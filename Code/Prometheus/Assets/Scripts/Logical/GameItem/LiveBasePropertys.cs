948b0d576e39b030074f6430bc4a0eff98f9f177
青鑫 <amostalong@126.com>
2017-12-28 21:33:27 +0800

    青鑫：新版本属性的一部分

--- Code/Prometheus/Assets/Scripts/Logical/GameItem/LiveBasePropertys.cs
+++ Code/Prometheus/Assets/Scripts/Logical/GameItem/LiveBasePropertys.cs
@@ -34,5 +34,35 @@ public class LiveBasePropertys : PropertyData {
         return this;
     }
 
-    //public float ohp, ospeed, omelee, olaser, ocartridge, oatk, omoto, ocapacity, oatkspeed, oreloadspeed, oshield, oarmor;
+    public LiveBasePropertys InitMonsterProperty(
+        float hp,
+        float atk,
+        float shield,
+        float armor)
+    {
+        SetFloatProperty(GameProperty.mhp, hp);
+        SetFloatProperty(GameProperty.attack, atk);
+        SetFloatProperty(GameProperty.shield, shield);
+        SetFloatProperty(GameProperty.guard, armor);
+  
+        return this;
+    }
+
+    public LiveBasePropertys InitPlayerProperty(
+        float hp,
+        float fatk,
+        float melee,
+        float laser,
+        float shield,
+        float armor)
+    {
+        SetFloatProperty(GameProperty.mhp, hp);
+        SetFloatProperty(GameProperty.firstAtt, fatk);
+        SetFloatProperty(GameProperty.melee, melee);
+        SetFloatProperty(GameProperty.shield, shield);
+        SetFloatProperty(GameProperty.guard, armor);
+
+        return this;
+    }
+
 }
