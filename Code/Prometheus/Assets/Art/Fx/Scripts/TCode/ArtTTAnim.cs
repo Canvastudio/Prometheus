b8ff7c452da29d502bd254aee6932d590466b1e5
Canvas <444569546@qq.com>
2017-12-30 01:01:46 +0800

    添加 属性变化 效果资源。

---
+++ Code/Prometheus/Assets/Art/Fx/Scripts/TCode/ArtTTAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtTTAnim : MonoBehaviour {
+
+	public Animator m_anim;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.A))
+			m_anim.Play("anim_atk");
+
+		if(Input.GetKeyDown(KeyCode.S))
+			m_anim.Play("anim_hp");
+
+		if(Input.GetKeyDown(KeyCode.W))
+			m_anim.Play("anim_hudun");
+
+		if(Input.GetKeyDown(KeyCode.E))
+			m_anim.Play("anim_hujia");		
+	}
+}
